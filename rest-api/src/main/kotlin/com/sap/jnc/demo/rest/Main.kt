7bdc093091562724f9cb242a95f97bff6545abf2
Vic Lau <xingyulliiuu@gmail.com>
2016-06-07 16:50:52 +0800

    use static import

--- rest-api/src/main/kotlin/com/sap/jnc/demo/rest/Main.kt
+++ rest-api/src/main/kotlin/com/sap/jnc/demo/rest/Main.kt
@@ -1,13 +1,13 @@
 package com.sap.jnc.demo.rest
 
 import com.google.gson.Gson
-import spark.Spark
 import spark.Spark.halt
+import spark.Spark.post
 
 fun main(args: Array<String>) {
     clients["jnc"] = Client("jnc", "123456")
 
-    Spark.post("/api/client_auth") { req, res ->
+    post("/api/client_auth") { req, res ->
         val body = Gson().fromJson(req.body(), Map::class.java)
         val clientid = body["clientid"]?.toString()
         val clientsecret = body["clientsecret"]?.toString()
@@ -26,7 +26,7 @@ fun main(args: Array<String>) {
         return@post Gson().toJson(generateToken(clientid))
     }
 
-    Spark.post("/api/erp/order") { req, res ->
+    post("/api/erp/order") { req, res ->
         val input = Gson().fromJson(req.body(), OrderApiInput::class.java)
 
         if (input.token == null || !checkToken(input.token)) {
